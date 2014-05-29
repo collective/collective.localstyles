064a6406242da25c24fac58fe445af26d78f1f29
Johannes Raggam <raggam-nl@adm.at>
2014-05-29 15:35:53 +0200

    render url in python, rename localstyle_file to localstyles_url, test if context is a folder

--- collective/localstyles/styleincluder_viewlet.py
+++ collective/localstyles/styleincluder_viewlet.py
@@ -1,7 +1,7 @@
 from Acquisition import aq_parent
 from plone.app.layout.viewlets.common import ViewletBase
 from zope.component.interfaces import ISite
-
+from plone.folder.interfaces import IFolder
 
 LOCALSTYLES_FILE = 'localstyles.css'
 
@@ -9,11 +9,11 @@ LOCALSTYLES_FILE = 'localstyles.css'
 class StyleIncluderViewlet(ViewletBase):
 
     @property
-    def localstyle_file(self):
+    def localstyles_url(self):
         context = self.context
 
         def _get_localstyles(context):
-            if LOCALSTYLES_FILE in context:
+            if IFolder.providedBy(context) and LOCALSTYLES_FILE in context:
                 return context[LOCALSTYLES_FILE]
 
             elif ISite.providedBy(context):
@@ -24,4 +24,5 @@ class StyleIncluderViewlet(ViewletBase):
                 # Try to get localstyles file from parent
                 return _get_localstyles(aq_parent(context))
 
-        return _get_localstyles(context)
+        localstyles = _get_localstyles(context)
+        return localstyles.absolute_url() if localstyles else None
