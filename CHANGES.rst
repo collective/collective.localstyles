2f32434043a4bd25146340967682364519b9ce24
Johannes Raggam <thetetet@gmail.com>
2016-05-09 11:51:49 +0200

    Revert "Changed module-global ``LOCALSTYLES_FILE`` to ``LOCALSTYLES_FILES``, which now holds a list of local style file names."
    This reverts commit e7313cec432ed70ccdf20991b046d59223e3c070.

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,15 +4,7 @@ Changelog
 1.1 (unreleased)
 ----------------
 
-Incompatibilities:
-
-- Changed module-global ``LOCALSTYLES_FILE`` to ``LOCALSTYLES_FILES``, which now holds a list of local style file names.
-  [thet]
-
-New:
-
-- Simplified aggregation of local style file to make use of acquisition.
-  [thet]
+- Nothing changed yet.
 
 
 1.0 (2014-06-03)
--- collective/localstyles/styleincluder_viewlet.py
+++ collective/localstyles/styleincluder_viewlet.py
@@ -1,6 +1,9 @@
+from Acquisition import aq_parent
 from plone.app.layout.viewlets.common import ViewletBase
+from zope.component.interfaces import ISite
+from plone.folder.interfaces import IFolder
 
-LOCALSTYLES_FILES = ['localstyles.css', 'localstyles_css']
+LOCALSTYLES_FILE = 'localstyles.css'
 
 
 class StyleIncluderViewlet(ViewletBase):
@@ -9,7 +12,17 @@ class StyleIncluderViewlet(ViewletBase):
     def localstyles_url(self):
         context = self.context
 
-        for it in LOCALSTYLES_FILES:
-            style_file = context.get(it, None)
-            if style_file:
-                return style_file.absolute_url()
+        def _get_localstyles(context):
+            if IFolder.providedBy(context) and LOCALSTYLES_FILE in context:
+                return context[LOCALSTYLES_FILE]
+
+            elif ISite.providedBy(context):
+                # Stop traversing at ISite boundaries
+                return None
+
+            else:
+                # Try to get localstyles file from parent
+                return _get_localstyles(aq_parent(context))
+
+        localstyles = _get_localstyles(context)
+        return localstyles.absolute_url() if localstyles else None
